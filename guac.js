1f7486fb1590c3cae7482d936511a952e1012d47
Hex <me@hexplo.it>
2024-07-03 17:07:50 +0100

    Update guac.js

--- guac.js
+++ guac.js
@@ -1,1 +1,7 @@
 console.log('guac.js loaded')
+document.title = "r2frida - training lab"
+
+const appName = document.getElementsByClassName("app-name")
+if (appName) {
+  appName.innerHTML = "r2frida labs"
+}
