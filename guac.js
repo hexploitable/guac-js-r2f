5ef71dff9fd6f29b944c3e6c7c546cc1a748820a
Hex <me@hexplo.it>
2024-07-03 20:49:34 +0100

    Update guac.js

--- guac.js
+++ guac.js
@@ -1,11 +1,11 @@
 console.log('guac.js loaded')
 
-window.onload = function() {
-  document.title = "r2frida - training lab"
+window.onload = () => {
+  document.title = 'r2frida - training lab'
 
-  const appName = document.getElementsByClassName("app-name")
+  const appName = document.getElementsByClassName('app-name')
   if (appName) {
-    appName.innerHTML = "r2frida labs"
+    appName.innerHTML = 'r2frida labs'
   }
   
   console.log('page updated')
